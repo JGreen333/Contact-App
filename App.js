7f9cf76356bcf7203313f8f18c3933f3393eecb2
Julian <114091481+JGreen333@users.noreply.github.com>
2022-10-15 17:19:13 -0400

    Add files via upload

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import ContactCard from "./ContactCard";
+
+const App = () => {
+  const [results, setResults] = useState([]);
+  useEffect(() => {
+    fetch("https://randomuser.me/api/?results=5")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+        setResults(data.results);
+      });
+  }, []);
+
+  return (
+    // RETURNING FROM CONTACTCARD
+    <div>
+      {results.map((result, index) => {
+        return (
+          <ContactCard
+            key={index}
+            avatarUrl={result.picture.large}
+            name={result.name.first + " " + result.name.last}
+            email={result.email}
+            age={result.dob.age}
+          />
+        );
+      })}
+    </div>
+  );
+};
+
+export default App;
